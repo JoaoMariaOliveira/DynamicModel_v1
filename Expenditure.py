e27761fb85cd660268557edb15604e0d2f5a8c63
Francis Tseng <f+accounts@frnsys.com>
2018-10-06 01:03:04 +0800

    using cython funcs

--- Expenditure.py
+++ Expenditure.py
@@ -10,7 +10,7 @@ def kron(a, nrows):
     return np.repeat(a, nrows * np.ones(a.shape[0], np.int), axis=0)
 
 import numpy as np
-from cfuncs import ExpenditureAux, OM_sum_f
+from cfuncs import ExpenditureAux, OM_sum_f, compute_IA, compute_NBP
 
 def OM_sum(GG, NNBP, IA, nSectors, nCountries):
     GP = GG * NNBP
@@ -18,28 +18,39 @@ def OM_sum(GG, NNBP, IA, nSectors, nCountries):
     OM = I - (GP + IA)
     return OM
 
+# def compute_IA(mWeightedTariffs, mAlphas, nSectors, nCountries):
+#     IA = np.zeros([nSectors * nCountries, nSectors * nCountries], dtype=float)
+#     I_F = 1 - mWeightedTariffs
+
+#     for n in range(nCountries):
+#         IA[n * nSectors: (n + 1) * nSectors, n * nSectors: (n + 1) * nSectors] =  np.kron(mAlphas[:, n], (I_F[:, n].T)).reshape(nSectors, nSectors)
+#     return IA
+
+# def compute_NBP(mTradeShare, mTauActual, mShareVA, nSectors, nCountries):
+#     Pit = mTradeShare / mTauActual
+#     Bt = 1 - mShareVA
+#     BP = np.zeros([nSectors * nCountries, nCountries], dtype=float)
+
+#     for j in range(nSectors):
+#         BP[j * nCountries: (j + 1) * nCountries, :] = np.kron(np.ones(nCountries).reshape(nCountries, 1), Bt[j, :]) * Pit[j * nCountries: (j + 1) * nCountries, :]
+
+#     NBP = np.zeros([nCountries, nSectors * nCountries], dtype=float)
+
+#     for j in range(nCountries):
+#         for n in range(nCountries):
+#             NBP[j, n * nSectors: (n + 1) * nSectors] = BP[n: nSectors * nCountries: nCountries, j]
+#     return NBP
+
+
 @profile
 def Expenditure(mAlphas, mShareVA, mIO, mTradeShare, mTauActual, mWeightedTariffs, VAn, mWages, Sn, nSectors, nCountries,
                 LG, VA_Br, mWagesBrasil, nPositionBR, PQ):
 
-    IA = np.zeros([nSectors * nCountries, nSectors * nCountries], dtype=float)
-    I_F = 1 - mWeightedTariffs
-
-    for n in range(nCountries):
-        IA[n * nSectors: (n + 1) * nSectors, n * nSectors: (n + 1) * nSectors] =  np.kron(mAlphas[:, n], (I_F[:, n].T)).reshape(nSectors, nSectors)
+    IA = compute_IA(mWeightedTariffs, mAlphas, nSectors, nCountries)
 
     Pit = mTradeShare / mTauActual
     Bt = 1 - mShareVA
-    BP = np.zeros([nSectors * nCountries, nCountries], dtype=float)
-
-    for j in range(nSectors):
-        BP[j * nCountries: (j + 1) * nCountries, :] = np.kron(np.ones(nCountries).reshape(nCountries, 1), Bt[j, :]) * Pit[j * nCountries: (j + 1) * nCountries, :]
-
-    NBP = np.zeros([nCountries, nSectors * nCountries], dtype=float)
-
-    for j in range(nCountries):
-        for n in range(nCountries):
-            NBP[j, n * nSectors: (n + 1) * nSectors] = BP[n: nSectors * nCountries: nCountries, j]
+    NBP = compute_NBP(Pit, Bt, nSectors, nCountries)
 
     NNBP = kron(NBP, nSectors)
     # NNBP_old = np.kron(NBP, np.ones([nSectors, 1], dtype=float))
@@ -58,10 +69,10 @@ def Expenditure(mAlphas, mShareVA, mIO, mTradeShare, mTauActual, mWeightedTariff
 
     Vb = mAlphas * A
 
-    Vb = Vb.reshape(nSectors * nCountries, 1, order='F').copy()
+    Vb = Vb.reshape(nSectors * nCountries, 1, order='F')
     Bb = -mAlphas * (Sn * np.ones((1, nSectors))).T
-    Bb = Bb.reshape(nSectors * nCountries, 1, order='F').copy()
-    PQ_vec = PQ.T.reshape(nSectors * nCountries, 1, order='F').copy()
+    Bb = Bb.reshape(nSectors * nCountries, 1, order='F')
+    PQ_vec = PQ.T.reshape(nSectors * nCountries, 1, order='F')
 
     Soma = Vb + Bb
     PQ = ExpenditureAux(OM, Soma, PQ_vec)
